60f0abe7af09730c12494f99fe8d3ed1fb5b5ac8
pedrohenriquebr <pedrohenriquebraga735@gmail.com>
2021-01-20 09:45:06 -0300

    Add Items route to API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app  = express();
+const items = require('./items.json');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 require('dotenv').config();
 const port = process.env.PORT || 3000;
 
@@ -7,6 +10,11 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+//Items router
+app.route('/items')
+    .get((req, res) => {
+        res.send(items);
+    })
 
 app.listen(port, ()=>{
     console.log(`Listening at http://localhost:${port}`);
